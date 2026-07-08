6aa9b06d4d238d5b495515745f81fac71df13515
Den4ik-12 <133135758+DDen4ik-12@users.noreply.github.com>
2026-07-09 00:07:43 +0500

    pull.js: ➕ Create (WIP)

---
+++ scripts/pull.js
@@ -0,0 +1,68 @@
+import pathUtil from 'node:path';
+import fs from 'node:fs';
+
+/**
+ * @param {string} path
+ * @returns {boolean}
+ */
+const isDirectorySync = path => {
+    try {
+        const stat = fs.statSync(path);
+        return stat.isDirectory();
+    } catch (e) {
+        if (e.code === 'ENOENT') {
+            return false;
+        }
+        throw e;
+    }
+};
+
+/**
+ * @param {string} type
+ */
+const pullAssetsOfType = type => {
+    console.log(`Generating Dash ${type} library for scratch-gui...`);
+    const scratchGui = pathUtil.join(__dirname, '../../scratch-gui');
+    if (!isDirectorySync(scratchGui)) {
+        throw new Error('Could not find scratch-gui.');
+    }
+
+    const assetsDirectory = pathUtil.join(__dirname, `../${type}`);
+    if (!isDirectorySync(assetsDirectory)) {
+        console.log(`Skipping ${type}; could not find ${type}.`);
+        return;
+    }
+
+    const guiDashAssets = [];
+    const assetsFiles = fs.readdirSync(assetsDirectory)
+        .filter(name => pathUtil.extname(name) !== '.json');
+    for (const assetFile of assetsFiles) {
+        const metadataFile = pathUtil.join(assetsDirectory, `${pathUtil.parse(assetFile).name}.json`);
+        let rawMetadata;
+        try {
+            rawMetadata = fs.readFileSync(metadataFile);
+        } catch (e) {
+            throw new Error(`Failed to read metadata of ${pathUtil.parse(assetFile).name} (${type}).`);
+        }
+        let jsonMetadata;
+        try {
+            jsonMetadata = JSON.parse(rawMetadata);
+        } catch (e) {
+            throw new Error(`Invaild metadata of ${pathUtil.parse(assetFile).name} (${type}).`);
+        }
+    }
+        
+    const guiDashAssetsFile = pathUtil.join(scratchGui, `src/lib/libraries/dash-assets/generated-${type}.json`);
+    fs.writeFileSync(guiDashAssetsFile, JSON.stringify(guiDashAssets, null, 4));
+};
+
+const pullEverything = () => {
+    try {
+        pullAssetsOfType('backdrops');
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+};
+
+pullEverything();
