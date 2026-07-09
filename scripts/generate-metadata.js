df014961c11dd8234466dac19121e156db294882
Den4ik-12 <denisovmikhail2802@gmail.com>
2026-07-09 21:25:06 +0500

    generate-metadata.js: Improvements

--- scripts/generate-metadata.js
+++ scripts/generate-metadata.js
@@ -8,6 +8,17 @@ import {parseFile as getSoundMetadata} from 'music-metadata';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = pathUtil.dirname(__filename);
 
+const METADATA_PROPS_ORDER = [
+    'name',
+    'tags',
+    'bitmapResolution',
+    'dataFormat',
+    'rotationCenterX',
+    'rotationCenterY',
+    'sampleCount',
+    'rate'
+];
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
@@ -17,7 +28,7 @@ const rl = readline.createInterface({
  * @param {string} title
  * @returns {Promise<string>}
  */
-const prompt = title => new Promise((resolve) => rl.question(`${title} `, (answer) => resolve(answer)));
+const prompt = title => new Promise((resolve) => rl.question(`${title} `, answer => resolve(answer)));
 
 /**
  * @param {string} path
@@ -65,10 +76,13 @@ const genMetadata4AssetsOfType = async type => {
             throw new Error(`Invaild metadata of ${assetFilename} (${type}).`);
         }
 
+        let changed = false;
         if (!('name' in jsonMetadata)) {
+            changed = true;
             jsonMetadata.name = await prompt(`${assetFilename} (${type}): Enter the asset name... (empty for filename)`) || assetFilename;
         }
         if (!('tags' in jsonMetadata)) {
+            changed = true;
             const tagsString = await prompt(`${assetFilename} (${type}): Enter the tags separated by commas...`);
             jsonMetadata.tags = new Set(tagsString.split(',')).values().toArray();
         }
@@ -78,6 +92,7 @@ const genMetadata4AssetsOfType = async type => {
                 const dimensions = await imageSizeFromFile(pathUtil.join(assetsDirectory, assetFile));
 
                 if (!('bitmapResolution' in jsonMetadata)) {
+                    changed = true;
                     if (pathUtil.extname(assetFile) === '.svg') {
                         jsonMetadata.bitmapResolution = 1;
                     } else {
@@ -91,15 +106,18 @@ const genMetadata4AssetsOfType = async type => {
                     }
                 }
                 if (!('dataFormat' in jsonMetadata)) {
+                    changed = true;
                     jsonMetadata.dataFormat = pathUtil.extname(assetFile).slice(1).toLowerCase();
                 }
                 if (!('rotationCenterX' in jsonMetadata)) {
+                    changed = true;
                     const answer = await prompt(`${assetFilename} (${type}): Enter X of the rotation center... (empty for centered)`);
                     jsonMetadata.rotationCenterX = answer
                         ? parseFloat(answer)
                         : dimensions.width / 2;
                 }
                 if (!('rotationCenterY' in jsonMetadata)) {
+                    changed = true;
                     const answer = await prompt(`${assetFilename} (${type}): Enter Y of the rotation center... (empty for centered)`);
                     jsonMetadata.rotationCenterY = answer
                         ? parseFloat(answer)
@@ -111,20 +129,29 @@ const genMetadata4AssetsOfType = async type => {
                 const soundMetadata = await getSoundMetadata(pathUtil.join(assetsDirectory, assetFile));
 
                 if (!('dataFormat' in jsonMetadata)) {
+                    changed = true;
                     jsonMetadata.dataFormat = '';
                 }
                 if (!('sampleCount' in jsonMetadata)) {
+                    changed = true;
                     jsonMetadata.sampleCount = soundMetadata.format.numberOfSamples;
                 }
                 if (!('rate' in jsonMetadata)) {
+                    changed = true;
                     jsonMetadata.rate = soundMetadata.format.sampleRate;
                 }
                 break;
             }
         }
 
-        console.log(`Metadata result of ${assetFilename} (${type}):`, jsonMetadata);
-        fs.writeFileSync(metadataFile, JSON.stringify(jsonMetadata, null, 4));
+        if (changed) {
+            // Normalize order of metadata props
+            jsonMetadata = Object.fromEntries(Object.entries(jsonMetadata).sort(
+                ([prop1], [prop2]) => METADATA_PROPS_ORDER.indexOf(prop1) - METADATA_PROPS_ORDER.indexOf(prop2)
+            ));
+            console.log(`Metadata result of ${assetFilename} (${type}):`, jsonMetadata);
+            fs.writeFileSync(metadataFile, JSON.stringify(jsonMetadata, null, 4));
+        }
     }
 };
 
